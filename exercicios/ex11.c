634cb41881b61d9941804ed6027450a327bd304d
thiago5171 <tgazaroli@gmail.com>
2021-03-03 17:21:34 -0300

    Create ex11.c

---
+++ exercicios/ex11.c
@@ -0,0 +1,3 @@
+/*Desenvolva em cada LP um programa que calcule o preço a pagar pelo fornecimento de energia elétrica. 
+Pergunte a quantidade de kWh consumida e o tipo de instalação: R para residências, I para indústrias e C para 
+comércios. Calcule o preço a pagar de acordo com a tabela a seguir:*/
