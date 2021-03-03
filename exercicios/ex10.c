b56f229504bc8be0b0322fc8c1779b7d1692d8b7
thiago5171 <tgazaroli@gmail.com>
2021-03-03 17:21:31 -0300

    Update ex10.c

--- exercicios/ex10.c
+++ exercicios/ex10.c
@@ -2,7 +2,7 @@
 Pergunte a quantidade de kWh consumida e o tipo de instalação: R para residências, I para indústrias e C para 
 comércios. Calcule o preço a pagar de acordo com a tabela a seguir:*/
 #include<stdio.h>
-
+#include<ctype.h>
 int main(){
     float kwh,pagar;
     char instalacao;
@@ -11,36 +11,43 @@ int main(){
     scanf("%f",&kwh);
 
     printf("digite o tipo de instalacao: \nR para residEncias \nI para industrias  \nC para comercios\n");
-    scanf("%c",&instalacao);
+    scanf("%s",&instalacao);
     
-
-
-    if (instalacao == "R"){
-        if (kwh>500){
+    switch (toupper(instalacao)){
+    case 'R':{
+        if(kwh>500){
             pagar=kwh*0.65;
         }else{
             pagar=kwh*0.40;
         }
-    }else{
-        if(instalacao == "C"){
-            if(kwh>1000){
-                pagar = kwh*0.60;
-            }else{
-                pagar=kwh*0.55;
-            }
+        printf("o preco total a pagar foi de R$%.2f ",pagar);
+
+    }
+        break;
+    case 'I':{
+        if(kwh>5000){
+            pagar=kwh*0.55;
         }else{
-            if(instalacao == "I"){
-                if(kwh>5000){
-                    pagar=kwh*0.55;
-                }else{
-                    pagar=kwh*0.60;
-                }
-            }
+            pagar=kwh*0.60;
         }
+        printf("o preco total a pagar foi de R$%.2f ",pagar);
+
+    }
+        break;
+    case 'C':{
+        if(kwh>1000){
+            pagar = kwh*0.60;
+        }else{
+            pagar=kwh*0.55;
+        }
+        printf("o preco total a pagar foi de R$%.2f ",pagar);
+
+    }
+    default:printf("erro\n");
+        break;
     }
 
 
 
-    printf("o preco total a pagar foi de R$%.2f ",pagar);
-
+    return 0;
 }
