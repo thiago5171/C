325da791989d99f4be5130664f943f1365ca2cff
thiago5171 <Tgazaroli@gmail.com>
2021-03-11 18:55:20 -0300

    Create ex16.c

---
+++ exercicios/ex16.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int i ;
+    
+    printf("Contagem regressiva para lancamento de foguem...\n");
+    for(i =10;i>=0;i--){
+        printf("%i",i);  
+        if (i==0){    
+            break;
+        }
+}
+}
