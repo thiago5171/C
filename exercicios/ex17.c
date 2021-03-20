398bbc3948ff53df848e301a73b16db86e853c2e
thiago5171 <Tgazaroli@gmail.com>
2021-03-19 22:01:23 -0300

    Create ex17.c

---
+++ exercicios/ex17.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    int m,n,maior=0,menor=0,i1,i2,resto1=0,resto2=0 ;
+    float dg=0, dp=0;
+    scanf("%i%i",&n,&m;
+    
+    if(n>m){
+        maior=n;
+        menor=m;
+    }else{
+        menor=n;
+        maior=m;
+    }
+    if(n==m){
+        print("%i"),n;
+    }else{
+        for (i1=1; i < maior; i++)
+        {
+            dp=maior/i1;
+            resto1
+
+        }//chave do 1 for
+        
+
+    }//chave do else
+
+}//chave final
