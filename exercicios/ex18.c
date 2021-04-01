b3459ecb8499fcbac4d24b3c118bc53e4e362ad9
thiago5171 <Tgazaroli@gmail.com>
2021-04-01 16:58:18 -0300

    Update ex18.c

---
+++ exercicios/ex18.c
@@ -0,0 +1,35 @@
+/*Determinar quantas vogais tem dentro de uma determinada string dada e imprimir 
+a string sem as vogais.
+*/
+#include<stdio.h>
+
+int main(){
+    char nome, vogais[]="aeiou";
+    int i=0, cont1=0;
+    gets(nome)
+}
+
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char texto[20];
+    char textoSemVogais[20];
+    int i, j, contVogais;
+    printf("Digite um texto: ");
+    gets(texto);
+    for (i=0,j=0; i<strlen(texto);i++){
+         if (texto[i]=='a' || texto[i] == 'e' || texto[i] =='i' || texto[i]=='o' || texto[i]=='u'){
+            contVogais++;
+            continue;
+         }
+    textoSemVogais[j]=texto[i];
+    j++;
+    }
+printf("Quant. Vogais: %i\n",contVogais);
+printf("Texto Sem Vogais: %s",text
+printf("Texto Sem Vogais: %s",textoSemVogais);
+return 0;
+}
+*/
